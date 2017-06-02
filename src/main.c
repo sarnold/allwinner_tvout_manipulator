e7aff49ae14f563b213d00398630fd55f9e8c48c
Daniel Giritzer <daniel@giritzer.eu>
2017-06-02 21:07:37 +0200

    Use reference in testdriver

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 
 int main()
 {
-    unsigned long *readfrommemory = 0;
+    unsigned long readfrommemory = 0;
 
-    readmem("0x01E00130", 'w', readfrommemory);
+    readmem("0x01E00130", 'w', &readfrommemory);
 
     printf("Value: 0x%X \n", (unsigned int)readfrommemory);
     return 0;
